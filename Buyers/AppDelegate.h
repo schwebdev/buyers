a805ee809dabc5cb03de1b112641a6cd2dca50ce
webdevelopment <webdevelopment@webdevelopments-Mac-mini.local>
2014-08-04 08:14:07 -0700

    Initial Commit

---
+++ Buyers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Buyers
+//
+//  Created by webdevelopment on 04/08/2014.
+//  Copyright (c) 2014 schuh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Buyers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Buyers
+//
+//  Created by webdevelopment on 04/08/2014.
+//  Copyright (c) 2014 schuh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
